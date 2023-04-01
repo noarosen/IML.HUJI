554964f99d1b34edfa0acf4c483b8d436ca621bf
noarosen <noa.rosen@mail.huji.ac.il>
2023-04-01 11:39:18 +0300

    added tests and ex1

--- IMLearn/learners/gaussian_estimators.py
+++ IMLearn/learners/gaussian_estimators.py
@@ -51,8 +51,13 @@ class UnivariateGaussian:
         Sets `self.mu_`, `self.var_` attributes according to calculated estimation (where
         estimator is either biased or unbiased). Then sets `self.fitted_` attribute to `True`
         """
-        raise NotImplementedError()
-
+        self.mu_ = X.mean()
+        if not self.biased_:  # if not biased = false
+            m = len(X) - 1
+        else:  # true
+            m = len(X)
+        x_sum_mu = np.sum((X - self.mu_) ** 2)
+        self.var_ = x_sum_mu / m
         self.fitted_ = True
         return self
 
@@ -76,7 +81,10 @@ class UnivariateGaussian:
         """
         if not self.fitted_:
             raise ValueError("Estimator must first be fitted before calling `pdf` function")
-        raise NotImplementedError()
+        x_exp = np.exp(- (X - self.mu_) ** 2 / (2 * self.var_))
+        x_sqrt = np.sqrt(2 * np.pi * self.var_)
+        pdfs = x_exp / x_sqrt
+        return pdfs
 
     @staticmethod
     def log_likelihood(mu: float, sigma: float, X: np.ndarray) -> float:
@@ -97,13 +105,14 @@ class UnivariateGaussian:
         log_likelihood: float
             log-likelihood calculated
         """
-        raise NotImplementedError()
+        log_likelihood = - ((1 / (2 * sigma)) * np.sum((X - mu) ** 2) + (len(X) / 2) * np.log(2 * np.pi * sigma))
+        return log_likelihood
 
 
 class MultivariateGaussian:
     """
-    Class for multivariate Gaussian Distribution Estimator
-    """
+        Class for multivariate Gaussian Distribution Estimator
+        """
     def __init__(self):
         """
         Initialize an instance of multivariate Gaussian estimator
@@ -143,8 +152,9 @@ class MultivariateGaussian:
         Sets `self.mu_`, `self.cov_` attributes according to calculated estimation.
         Then sets `self.fitted_` attribute to `True`
         """
-        raise NotImplementedError()
-
+        self.mu_ = X.mean(axis=0)  # rows
+        x_size = len(X) - 1
+        self.cov_ = np.dot(np.transpose(X - self.mu_), (X - self.mu_)) / x_size
         self.fitted_ = True
         return self
 
@@ -166,9 +176,12 @@ class MultivariateGaussian:
         ------
         ValueError: In case function was called prior fitting the model
         """
-        if not self.fitted_:
+        if not self.fitted_:  #
             raise ValueError("Estimator must first be fitted before calling `pdf` function")
-        raise NotImplementedError()
+        d = X[:, np.newaxis, :] - self.mu_
+        x_sum = np.sum(d.dot(inv(self.cov_)) * d, axis=2).flatten()
+        x_sqrt = np.sqrt((2 * np.pi) ** len(self.mu_) * det(self.cov_))
+        return np.exp(-0.5 * x_sum) / x_sqrt
 
     @staticmethod
     def log_likelihood(mu: np.ndarray, cov: np.ndarray, X: np.ndarray) -> float:
@@ -189,4 +202,9 @@ class MultivariateGaussian:
         log_likelihood: float
             log-likelihood calculated over all input data and under given parameters of Gaussian
         """
-        raise NotImplementedError()
+        m = len(X)
+        d = X[:, np.newaxis, :] - mu
+        l1 = np.log(2 * np.pi) * mu.shape[0] + slogdet(cov)[1]
+        l2 = np.sum(d.dot(inv(cov)) * d)
+        log_likelihood = - (m / 2) * l1 - 0.5 * l2
+        return log_likelihood
--- exercises/fit_gaussian_estimators.py
+++ exercises/fit_gaussian_estimators.py
@@ -2,29 +2,71 @@ from IMLearn.learners import UnivariateGaussian, MultivariateGaussian
 import numpy as np
 import plotly.graph_objects as go
 import plotly.io as pio
+
 pio.templates.default = "simple_white"
 
 
 def test_univariate_gaussian():
     # Question 1 - Draw samples and print fitted model
-    raise NotImplementedError()
+    mu = 10
+    var = 1
+    x = np.random.normal(mu, var, 1000)
+
+    fitted = UnivariateGaussian().fit(x)
+    print((np.round(fitted.mu_, 3), np.round(fitted.var_, 3)))
 
     # Question 2 - Empirically showing sample mean is consistent
-    raise NotImplementedError()
+    mu_plot = [np.abs(mu - UnivariateGaussian().fit(x[:n]).mu_)
+               for n in np.arange(var, len(x) / mu, dtype=np.int32) * mu]
+
+    x_size = len(mu_plot)
+    title1 = "Expectation Value Error as Function of Sample Size"
+    go.Figure(go.Scatter(x=list(range(x_size)), y=mu_plot, mode="markers",
+                         marker=dict(color="cornflowerblue")),
+              layout=dict(template=pio.templates.default, title=title1,
+                          xaxis_title=r"$\text{Sample size }$",
+                          yaxis_title=r"$\text{Sample mean estimator}$")) \
+        .write_image("PlotQ2.png")
 
     # Question 3 - Plotting Empirical PDF of fitted model
-    raise NotImplementedError()
+    pdf = np.c_[x, fitted.pdf(x)]  # adding feature of pdf to X
+    title2 = "PDF Plot - Fitted Model"
+    go.Figure(go.Scatter(x=pdf[:, 0], y=pdf[:, 1], mode="markers",
+                         marker=dict(color="cornflowerblue")),
+              layout=dict(template=pio.templates.default, title=title2,
+                          xaxis_title=r"$\text{Data value of fitted model}$",
+                          yaxis_title=r"$\text{PDF value}$")) \
+        .write_image("PlotQ3PDF.png")
 
 
 def test_multivariate_gaussian():
     # Question 4 - Draw samples and print fitted model
-    raise NotImplementedError()
+    mu = np.array([0, 0, 4, 0])
+    cov = np.array([[1, 0.2, 0, 0.5], [0.2, 2, 0, 0], [0, 0, 1, 0], [0.5, 0, 0, 1]])
+    x = np.random.multivariate_normal(mu, cov, 1000)
+    fitted = MultivariateGaussian().fit(x)
+    print(np.round(fitted.mu_, 3))
+    print(np.round(fitted.cov_, 3))
 
     # Question 5 - Likelihood evaluation
-    raise NotImplementedError()
+    matrix = np.zeros((200, 200))
+    values = np.linspace(-10, 10, matrix.shape[0])  # log likelihood
+    for i, f1 in enumerate(values):
+        for j, f3 in enumerate(values):
+            ll_ij = MultivariateGaussian.log_likelihood(np.array([f1, 0, f3, 0]), cov, x)
+            matrix[i, j] = np.round(ll_ij, 3)
+
+    title3 = "Log-Likelihood as Function of Expectation of Features 1,3"
+    go.Figure(go.Heatmap(x=values, y=values, z=matrix),
+              layout=dict(template=pio.templates.default, title=title3,
+                          xaxis_title=r"$\text{Mu by feature 3}$",
+                          yaxis_title=r"$\text{Mu by feature 1}$")) \
+        .write_image("PlotQ5HEATMAP.png")
 
     # Question 6 - Maximum likelihood
-    raise NotImplementedError()
+    max_index = list(np.unravel_index(matrix.argmax(), matrix.shape))
+    max_ll = values[max_index]
+    print("The model that achieved tha maximum log-likelihood is: ", np.round(max_ll, 3))
 
 
 if __name__ == '__main__':
